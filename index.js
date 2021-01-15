583108a2ee442909e424451556e2fa9d7a4ed29a
Rahil <rahil.eliyev@gmail.com>
2021-01-15 14:53:23 +0400

    hamburger menu, responsivnes

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-const x = {
-  name: "Rahil",
-  surname: "Ali"
-};
+const toggleOpenMenu = document.querySelector(".hamburger");
+const toggleCloseMenu = document.querySelector(".closing");
+const menu = document.getElementById("menu");
+let condition = false;
+toggleOpenMenu.addEventListener("click", () => {
+  if (condition == false) {
+    menu.style.opacity = 1;
+    menu.style.width = "100%";
+    condition = true;
+  } else {
+    menu.style.opacity = 0;
+    menu.style.width = "0%";
+    condition = false;
+  }
+});
+toggleCloseMenu.addEventListener("click", () => {
+  menu.style.opacity = 0;
+  menu.style.width = "0%";
+});
